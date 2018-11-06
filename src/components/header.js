ac1d514a142d6e8083aa71f4192f9b51c3bf6bde
Blake Noll <nollblake@gmail.com>
2018-11-06 10:05:40 -0800

    layout tweaks

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,12 @@ import styled from 'styled-components'
 
 const Header = ({ siteTitle, className}) => (
   <div className={className}>
-    <div
-      className="flex-row"
-    >
-      <h1 style={{ margin: 0 }}>
+    <div className="flex-row">
+      <h1 style={{ margin: 0, zIndex: 1 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
+            color: 'dodgerblue',
             textDecoration: 'none',
           }}
         >
@@ -23,11 +21,21 @@ const Header = ({ siteTitle, className}) => (
 )
 
 const StyledHeader = styled(Header)`
-  background: linear-gradient(to bottom right,rgba(196,202,208,.9),rgb(135,140,143));
+  background: rgba(0, 0, 0, .48);
   box-shadow: 0 1px 4px rgba(0,0,0,.16);
   position: fixed;
   width: 100%;
   z-index: 100;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-top: 47px solid #fff;
+    border-right: 47px solid transparent;
+    width: 40%;
+  }
 
   .flex-row {
     display: flex;
@@ -37,6 +45,12 @@ const StyledHeader = styled(Header)`
     padding: .5rem 1.0875rem;
     h1 {
       font-size: 1.5rem;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    &::before {
+      width: 60%;
     }
   }
 `
