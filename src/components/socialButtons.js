8182499f703f0af1f509eb877b45015b235bf412
Blake Noll <nollblake@gmail.com>
2019-05-17 11:54:34 -0700

    accesibility issues fixed

--- src/components/socialButtons.js
+++ src/components/socialButtons.js
@@ -3,14 +3,14 @@ import { SocialButton } from './styles'
 
 const SocialButtons = () => (
   <>
-    <SocialButton href="https://github.com/blakenoll" alt="github">
-      <i className="fa fa-github" />
+    <SocialButton href="https://github.com/blakenoll">
+      <i className="fa fa-github" alt="github" />
     </SocialButton>
-    <SocialButton href="https://www.linkedin.com/in/blake-noll" alt="linkedin">
-      <i className="fa fa-linkedin" />
+    <SocialButton href="https://www.linkedin.com/in/blake-noll">
+      <i className="fa fa-linkedin" alt="linkedin" />
     </SocialButton>
-    <SocialButton href="mailto:nollblake@gmail.com" alt="email">
-      <i className="fa fa-envelope-square" />
+    <SocialButton href="mailto:nollblake@gmail.com">
+      <i className="fa fa-envelope-square" alt="email" />
     </SocialButton>
   </>
 )
