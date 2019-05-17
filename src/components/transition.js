85e5d683a01ced5a29a02f6799bf8a11de6bdb3f
Blake Noll <nollblake@gmail.com>
2019-05-16 22:12:46 -0700

    removed margin top

--- src/components/transition.js
+++ src/components/transition.js
@@ -6,7 +6,6 @@ const TransitionComp = ({ children }) => {
   const defaultStyle = {
     transition: `filter ${duration}ms ease-out`,
     filter: 'blur(5px)',
-    marginTop: '50px',
   }
 
   const transitionStyles = {
